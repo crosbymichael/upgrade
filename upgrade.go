5cc36b62527939447a9c693341e4ac4f2167dc09
Michael Crosby <crosbymichael@gmail.com>
2017-06-30 13:28:11 -0700

    Finished spec upgrade

--- upgrade.go
+++ upgrade.go
@@ -65,7 +65,21 @@ func mapSpec(old *rc3.Spec) (*specs.Spec, error) {
 		Path:     old.Root.Path,
 		Readonly: old.Root.Readonly,
 	}
-	spec.Process = &specs.Process{
+	spec.Process = mapProcess(old)
+	if old.Linux != nil {
+		spec.Linux = mapLinux(old.Linux, spec.Process)
+	}
+	if old.Windows != nil {
+		// we don't need to map windows because it does not use runc or containerd
+	}
+	if old.Solaris != nil {
+		// we don't do solaris anymore
+	}
+	return &spec, nil
+}
+
+func mapProcess(old *rc3.Spec) *specs.Process {
+	process := &specs.Process{
 		Terminal:        old.Process.Terminal,
 		Args:            old.Process.Args,
 		Env:             old.Process.Env,
@@ -82,31 +96,19 @@ func mapSpec(old *rc3.Spec) (*specs.Spec, error) {
 		NoNewPrivileges: old.Process.NoNewPrivileges,
 	}
 	for _, r := range old.Process.Rlimits {
-		spec.Process.Rlimits = append(spec.Process.Rlimits, specs.LinuxRlimit{
+		process.Rlimits = append(process.Rlimits, specs.LinuxRlimit{
 			Type: r.Type,
 			Hard: r.Hard,
 			Soft: r.Soft,
 		})
 	}
 	if old.Process.ConsoleSize.Height != 0 || old.Process.ConsoleSize.Width != 0 {
-		spec.Process.ConsoleSize = &specs.Box{
+		process.ConsoleSize = &specs.Box{
 			Width:  old.Process.ConsoleSize.Width,
 			Height: old.Process.ConsoleSize.Height,
 		}
 	}
-	if old.Linux != nil {
-		if old.Linux.Resources != nil {
-			spec.Process.OOMScoreAdj = old.Linux.Resources.OOMScoreAdj
-		}
-	}
-	if old.Windows != nil {
-
-	}
-	if old.Solaris != nil {
-
-	}
-
-	return &spec, nil
+	return process
 }
 
 func mapHook(h rc3.Hook) specs.Hook {
@@ -116,6 +118,218 @@ func mapHook(h rc3.Hook) specs.Hook {
 		Env:     h.Env,
 		Timeout: h.Timeout,
 	}
+}
+
+func mapLinux(old *rc3.Linux, process *specs.Process) *specs.Linux {
+	linux := &specs.Linux{
+		Sysctl:            old.Sysctl,
+		MountLabel:        old.MountLabel,
+		MaskedPaths:       old.MaskedPaths,
+		ReadonlyPaths:     old.ReadonlyPaths,
+		RootfsPropagation: old.RootfsPropagation,
+	}
+	if old.CgroupsPath != nil {
+		linux.CgroupsPath = *old.CgroupsPath
+	}
+	for _, d := range old.Devices {
+		linux.Devices = append(linux.Devices, specs.LinuxDevice{
+			Path:     d.Path,
+			Type:     d.Type,
+			Major:    d.Major,
+			Minor:    d.Minor,
+			FileMode: d.FileMode,
+			UID:      d.UID,
+			GID:      d.GID,
+		})
+	}
+	for _, i := range old.UIDMappings {
+		linux.UIDMappings = append(linux.UIDMappings, mapID(i))
+	}
+	for _, i := range old.GIDMappings {
+		linux.GIDMappings = append(linux.GIDMappings, mapID(i))
+	}
+	for _, n := range old.Namespaces {
+		linux.Namespaces = append(linux.Namespaces, specs.LinuxNamespace{
+			Type: specs.LinuxNamespaceType(n.Type),
+			Path: n.Path,
+		})
+	}
+	if old.Seccomp != nil {
+		linux.Seccomp = mapSeccomp(old.Seccomp)
+	}
+	if old.Resources != nil {
+		linux.Resources = mapResources(old.Resources, process)
+	}
+	// no need to map IntelRdt
+	return linux
+}
+
+func mapSeccomp(old *rc3.Seccomp) *specs.LinuxSeccomp {
+	var s specs.LinuxSeccomp
+	s.DefaultAction = specs.LinuxSeccompAction(old.DefaultAction)
+	for _, a := range old.Architectures {
+		s.Architectures = append(s.Architectures, specs.Arch(a))
+	}
+	for _, a := range old.Syscalls {
+		sys := specs.LinuxSyscall{
+			Names:  []string{a.Name},
+			Action: specs.LinuxSeccompAction(a.Action),
+		}
+		for _, arg := range a.Args {
+			sys.Args = append(sys.Args, specs.LinuxSeccompArg{
+				Index:    arg.Index,
+				Value:    arg.Value,
+				ValueTwo: arg.ValueTwo,
+				Op:       specs.LinuxSeccompOperator(arg.Op),
+			})
+		}
+		s.Syscalls = append(s.Syscalls, sys)
+	}
+	return &s
+}
+
+func mapID(old rc3.IDMapping) specs.LinuxIDMapping {
+	return specs.LinuxIDMapping{
+		HostID:      old.HostID,
+		ContainerID: old.ContainerID,
+		Size:        old.Size,
+	}
+}
+
+func defaultString(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
+
+func toInt(i *uint64) *int64 {
+	if i == nil {
+		return nil
+	}
+	ii := *i
+	iv := int64(ii)
+	return &iv
+}
+
+func mapResources(old *rc3.Resources, process *specs.Process) *specs.LinuxResources {
+	r := &specs.LinuxResources{}
+	// this field moved from resources to the process
+	process.OOMScoreAdj = old.OOMScoreAdj
+
+	r.DisableOOMKiller = old.DisableOOMKiller
+	for _, d := range old.Devices {
+		r.Devices = append(r.Devices, specs.LinuxDeviceCgroup{
+			Allow:  d.Allow,
+			Type:   defaultString(d.Type),
+			Major:  d.Major,
+			Minor:  d.Minor,
+			Access: defaultString(d.Access),
+		})
+	}
+	if old.Memory != nil {
+		m := old.Memory
+		r.Memory = &specs.LinuxMemory{
+			Limit:       toInt(m.Limit),
+			Reservation: toInt(m.Reservation),
+			Swap:        toInt(m.Swap),
+			Kernel:      toInt(m.Kernel),
+			KernelTCP:   toInt(m.KernelTCP),
+			// TODO: fix -1 here
+			Swappiness: m.Swappiness,
+		}
+	}
+	if old.CPU != nil {
+		c := old.CPU
+		r.CPU = &specs.LinuxCPU{
+			Shares:          c.Shares,
+			Quota:           toInt(c.Quota),
+			Period:          c.Period,
+			RealtimeRuntime: toInt(c.RealtimeRuntime),
+			RealtimePeriod:  c.RealtimePeriod,
+			Cpus:            defaultString(c.Cpus),
+			Mems:            defaultString(c.Mems),
+		}
+	}
+	if old.Pids != nil {
+		var (
+			p  = old.Pids
+			nv int64
+		)
+		if p.Limit != nil {
+			nv = *p.Limit
+		}
+		r.Pids = &specs.LinuxPids{
+			Limit: nv,
+		}
+	}
+	if old.BlockIO != nil {
+		b := old.BlockIO
+		r.BlockIO = &specs.LinuxBlockIO{
+			Weight:                  b.Weight,
+			LeafWeight:              b.LeafWeight,
+			WeightDevice:            mapBlkioW(b.WeightDevice),
+			ThrottleReadBpsDevice:   mapBlkioT(b.ThrottleReadBpsDevice),
+			ThrottleWriteBpsDevice:  mapBlkioT(b.ThrottleWriteBpsDevice),
+			ThrottleReadIOPSDevice:  mapBlkioT(b.ThrottleReadIOPSDevice),
+			ThrottleWriteIOPSDevice: mapBlkioT(b.ThrottleWriteIOPSDevice),
+		}
+	}
+	for _, h := range old.HugepageLimits {
+		var nv uint64
+		if h.Limit != nil {
+			nv = *h.Limit
+		}
+		r.HugepageLimits = append(r.HugepageLimits, specs.LinuxHugepageLimit{
+			Pagesize: defaultString(h.Pagesize),
+			Limit:    nv,
+		})
+	}
+	if old.Network != nil {
+		n := old.Network
+		r.Network = &specs.LinuxNetwork{
+			ClassID:    n.ClassID,
+			Priorities: mapNetprio(n.Priorities),
+		}
+	}
+	return r
+}
+
+func mapNetprio(old []rc3.InterfacePriority) (out []specs.LinuxInterfacePriority) {
+	for _, n := range old {
+		out = append(out, specs.LinuxInterfacePriority{
+			Name:     n.Name,
+			Priority: n.Priority,
+		})
+	}
+	return out
+}
+
+func mapBlkioW(old []rc3.WeightDevice) (out []specs.LinuxWeightDevice) {
+	for _, o := range old {
+		n := specs.LinuxWeightDevice{}
+		n.Major = o.Major
+		n.Minor = o.Minor
+		n.Weight = o.Weight
+		n.LeafWeight = o.LeafWeight
+		out = append(out, n)
+	}
+	return out
+}
+
+func mapBlkioT(old []rc3.ThrottleDevice) (out []specs.LinuxThrottleDevice) {
+	for _, o := range old {
+		n := specs.LinuxThrottleDevice{}
+		n.Major = o.Major
+		n.Minor = o.Minor
+		var nv uint64
+		if o.Rate != nil {
+			nv = *o.Rate
+		}
+		n.Rate = nv
+		out = append(out, n)
+	}
+	return out
 }
 
 func rewriteJSON(path string, v interface{}) error {
